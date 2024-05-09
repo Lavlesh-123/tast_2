dba7053bde9359a8daa066e6d79cd6f212b3c950
lavlesh <lavleshkum01234@gmail.com>
2024-05-09 22:22:30 +0530

    addeddd

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,4 @@
-// import 'package:flutter/material.dart';
-// import 'package:task_2/view/home.dart';
-// void main() {
-//   runApp(const MyApp());
-// }
 
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       debugShowCheckedModeBanner: false,
-//       title: 'Flutter Demo',
-//       theme: ThemeData(
-//        fontFamily: 'app_font',
-//         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
-//         useMaterial3: true,
-//       ),
-//       home:   const HomeView(),
-//     );
-//   }
-// }
 
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
